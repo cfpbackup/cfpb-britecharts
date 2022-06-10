592f6a02308da4df566c7e415eb92a69d29b0327
Richard Dinh <richard.dinh@cfpb.gov>
2022-06-10 12:53:05 -0400

    put back whole bundle.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
+import bar from './charts/bar.js';
+import donut from './charts/donut.js';
+import legend from './charts/legend.js';
 import line from './charts/line.js';
+import loadingStates from './charts/helpers/load.js';
 import row from './charts/row.js';
 import tooltip from './charts/tooltip.js';
 import miniTooltip from './charts/mini-tooltip.js';
+import sparkline from './charts/sparkline.js';
 import stackedArea from './charts/stacked-area.js';
+import scatterPlot from './charts/scatter-plot.js';
+import groupedBar from './charts/grouped-bar.js';
 import groupedRow from './charts/grouped-row.js';
 import stackedBar from './charts/stacked-bar.js';
+import heatmap from './charts/heatmap.js';
+import step from './charts/step.js';
+import brush from './charts/brush.js';
+import bullet from './charts/bullet.js';
 import colors from './charts/helpers/color.js';
 
-// import bar from './charts/bar.js';
-// import donut from './charts/donut.js';
-// import legend from './charts/legend.js';
-// import loadingStates from './charts/helpers/load.js';
-// import sparkline from './charts/sparkline.js';
-// import scatterPlot from './charts/scatter-plot.js';
-// import groupedBar from './charts/grouped-bar.js';
-// import heatmap from './charts/heatmap.js';
-// import step from './charts/step.js';
-// import brush from './charts/brush.js';
-// import bullet from './charts/bullet.js';
-
 export {
-    // bar,
-    // donut,
-    // legend,
+    bar,
+    donut,
+    legend,
     line,
-    // loadingStates,
+    loadingStates,
     tooltip,
     miniTooltip,
     row,
-    // sparkline,
+    sparkline,
     stackedArea,
-    // scatterPlot,
-    // groupedBar,
+    scatterPlot,
+    groupedBar,
     groupedRow,
     stackedBar,
-    // heatmap,
-    // step,
-    // brush,
-    // bullet,
+    heatmap,
+    step,
+    brush,
+    bullet,
     colors
 };
