984b0c266710790e58bc2c15bf5ca20b8550c402
Richard Dinh <richard.dinh@cfpb.gov>
2022-06-13 14:42:04 -0400

    bug fixing missing d3 transition import for row chart

--- src/charts/row.js
+++ src/charts/row.js
@@ -10,6 +10,7 @@ define(function(require) {
     const d3Format = require('d3-format');
     const d3Scale = require('d3-scale');
     const d3Selection = require('d3-selection');
+    const d3Transition = require('d3-transition');
 
     const textHelper = require('./helpers/text');
     const {exportChart} = require('./helpers/export');
